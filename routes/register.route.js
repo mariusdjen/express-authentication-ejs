008fe22ad82bd461b369901742f6eaf08309ec86
Mariusdev <minhadjenontin@gmail.com>
2024-08-31 01:06:02 -0700

    Data retrieval with body parser

--- routes/register.route.js
+++ routes/register.route.js
@@ -1,8 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const router = express.Router();
 
 router.get('/register',(req,res)=>{
+    res.render('register');
+})
+router.post('/register',(req,res)=>{
+
+
+    console.log(req.body);
+    
     res.render('register');
 })
 export default router;
